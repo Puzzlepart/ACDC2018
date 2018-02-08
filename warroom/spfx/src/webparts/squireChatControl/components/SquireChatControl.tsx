a221ba1af944dd14f1f7ef40efa9c4146cc873b4
Kim Damsleth <kim@damsleth.com>
2018-02-08 19:38:15 +0100

    squirechat

--- warroom/spfx/src/webparts/squireChatControl/components/SquireChatControl.tsx
+++ warroom/spfx/src/webparts/squireChatControl/components/SquireChatControl.tsx
@@ -2,24 +2,17 @@ import * as React from 'react';
 import styles from './SquireChatControl.module.scss';
 import { ISquireChatControlProps } from './ISquireChatControlProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-
+import { Chat } from 'botframework-webchat';
 export default class SquireChatControl extends React.Component<ISquireChatControlProps, {}> {
   public render(): React.ReactElement<ISquireChatControlProps> {
     return (
       <div className={ styles.squireChatControl }>
         <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
+        <link href="https://cdn.botframework.com/botframework-webchat/latest/botchat.css" rel="stylesheet"></link>
+         <Chat directLine={{ secret: 'UYhZ8t536jA.cwA.53w.7zezXxQeKxaJuO9IGvBkPotyMqdyxIw_PCno3Xr_r7k' }} bot={{id: 'Podrick_Payne'}} user={{ id: 'KingInTheNorth', name: 'King in the North' }}/>
         </div>
       </div>
     );
   }
 }
+ 
