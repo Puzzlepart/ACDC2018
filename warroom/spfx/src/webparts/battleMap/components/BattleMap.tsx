7f9f27323bec3e89d5dfe80ac47ca73731a377f9
Kim Damsleth <kim@damsleth.com>
2018-02-09 23:39:04 +0100

    BattleMap

--- warroom/spfx/src/webparts/battleMap/components/BattleMap.tsx
+++ warroom/spfx/src/webparts/battleMap/components/BattleMap.tsx
@@ -3,28 +3,114 @@ import styles from './BattleMap.module.scss';
 import { IBattleMapProps } from './IBattleMapProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { CompoundButton } from 'office-ui-fabric-react';
-import leaflet from "leaflet";
+import { L } from "leaflet";
 import pnp from "sp-pnp-js";
 
 
 
 export default class BattleMap extends React.Component<IBattleMapProps, {}> {
+
   public render(): React.ReactElement<IBattleMapProps> {
     return (
       <div className={styles.battleMap}>
         <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint!</span>
-              <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
-              <p className={styles.description}>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Learn more</span>
-              </a>
-            </div>
+          <div id="map" className={styles.map}>
+
           </div>
         </div>
       </div>
     );
   }
+
+  public componentDidMount() {
+    this.loadMap();
+  }
+
+
+
+  public loadMap() {
+    var map = L.map('map', {
+      crs: L.CRS.Simple
+    });
+
+    var yx = L.latLng;
+
+    var xy = function (x, y) {
+      if (L.Util.isArray(x)) {    // When doing xy([x, y]);
+        return yx(x[10], x[0]);
+      }
+      return yx(y, x);  // When doing xy(x, y);
+    };
+
+    var bounds = [xy(0, 0), xy(3000, 4242)];
+    var image = L.imageOverlay('../SiteAssets/WesterosMap.png', bounds).addTo(map);
+
+    var KingsLanding = xy(1612, 1410.0);
+    var CrossRoadsInn = xy(1500, 1900.0);
+    var StormsEnd = xy(1900, 1060.0);
+
+    L.marker(CrossRoadsInn).addTo(map).bindPopup('Crossroads Inn');
+    L.marker(KingsLanding).addTo(map).bindPopup('Kings Landing');
+    L.marker(StormsEnd).addTo(map).bindPopup("Storm's End");
+
+    var travel = L.polyline([KingsLanding, CrossRoadsInn]).addTo(map);
+    var travel = L.polyline([KingsLanding, StormsEnd]).addTo(map);
+
+    map.setMaxZoom(2)
+    map.setMinZoom(-2)
+    map.setView(xy(1620, 1450), -2)
+
+    L.Control.MousePosition = L.Control.extend({
+      options: {
+        position: 'bottomleft',
+        separator: ' : ',
+        emptyString: 'Unavailable',
+        lngFirst: false,
+        numDigits: 5,
+        lngFormatter: undefined,
+        latFormatter: undefined,
+        prefix: ""
+      },
+
+      onAdd: function (map) {
+        this._container = L.DomUtil.create('div', 'leaflet-control-mouseposition');
+        L.DomEvent.disableClickPropagation(this._container);
+        map.on('mousemove', this._onMouseMove, this);
+        this._container.innerHTML = this.options.emptyString;
+        return this._container;
+      },
+
+      onRemove: function (map) {
+        map.off('mousemove', this._onMouseMove)
+      },
+
+      _onMouseMove: function (e) {
+        var lng = this.options.lngFormatter ? this.options.lngFormatter(e.latlng.lng) : L.Util.formatNum(e.latlng.lng, this.options.numDigits);
+        var lat = this.options.latFormatter ? this.options.latFormatter(e.latlng.lat) : L.Util.formatNum(e.latlng.lat, this.options.numDigits);
+        var value = this.options.lngFirst ? lng + this.options.separator + lat : lat + this.options.separator + lng;
+        var prefixAndValue = this.options.prefix + ' ' + value;
+        this._container.innerHTML = prefixAndValue;
+      }
+
+    });
+
+    L.Map.mergeOptions({
+      positionControl: false
+    });
+
+    L.Map.addInitHook(function () {
+      if (this.options.positionControl) {
+        this.positionControl = new L.Control.MousePosition();
+        this.addControl(this.positionControl);
+      }
+    });
+
+    L.control.mousePosition = function (options) {
+      return new L.Control.MousePosition(options);
+    };
+
+    L.control.mousePosition().addTo(map);
+
+  }
 }
+
