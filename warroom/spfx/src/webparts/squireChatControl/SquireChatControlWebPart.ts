260db520229b0a1876cb4152f68030229920238b
Kim Damsleth <kim@damsleth.com>
2018-02-09 01:51:45 +0100

    Motha fuckin UI-fabric chat client

--- warroom/spfx/src/webparts/squireChatControl/SquireChatControlWebPart.ts
+++ warroom/spfx/src/webparts/squireChatControl/SquireChatControlWebPart.ts
@@ -8,28 +8,29 @@ import {
 } from '@microsoft/sp-webpart-base';
 
 import * as strings from 'SquireChatControlWebPartStrings';
-import SquireChatControl from './components/SquireChatControl';
-import { ISquireChatControlProps } from './components/ISquireChatControlProps';
 
-export interface ISquireChatControlWebPartProps {
-  description: string;
-}
+import SquireChatControl, { ISquireChatControlProps } from './components/SquireChatControl';
+import { ISquireChatControlWebPartProps } from './ISquireChatControlWebPartProps';
 
 export default class SquireChatControlWebPart extends BaseClientSideWebPart<ISquireChatControlWebPartProps> {
 
   public render(): void {
-    const element: React.ReactElement<ISquireChatControlProps > = React.createElement(
-      SquireChatControl,
-      {
-        description: this.properties.description
-      }
+    const element: React.ReactElement<ISquireChatControlProps> = React.createElement(SquireChatControl, {
+      description: this.properties.description,
+      message: '',
+      title: this.properties.title,
+      placeholderText: this.properties.placeholderText,
+      directLineSecret: this.properties.directLineSecret,
+      titleBarBackgroundColor: this.properties.titleBarBackgroundColor,
+      botMessagesBackgroundColor: this.properties.botMessagesBackgroundColor,
+      botMessagesForegroundColor: this.properties.botMessagesForegroundColor,
+      userMessagesBackgroundColor: this.properties.userMessagesBackgroundColor,
+      userMessagesForegroundColor: this.properties.userMessagesForegroundColor,
+      context: this.context
+    }
     );
 
     ReactDom.render(element, this.domElement);
-  }
-
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -37,14 +38,50 @@ export default class SquireChatControlWebPart extends BaseClientSideWebPart<ISqu
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: 'Here you can set various properties and settings regarding how your bot chat web part will look visually and functionally work'
           },
           groups: [
             {
-              groupName: strings.BasicGroupName,
+              groupName: 'Bot Connection',
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField('directLineSecret', {
+                  label: 'Direct Line Secret'
+                })
+              ]
+            },
+            {
+              groupName: 'Appearance',
+              groupFields: [
+                PropertyPaneTextField('title', {
+                  label: 'Title'
+                }),
+                PropertyPaneTextField('placeholderText', {
+                  label: 'Placeholder text'
+                }),
+                PropertyPaneTextField('titleBarBackgroundColor', {
+                  label: 'Title bar background color',
+                  onGetErrorMessage: this._validateColorPropertyAsync.bind(this), // validation function
+                  deferredValidationTime: 500 // delay after which to run the validation function
+                }),
+                PropertyPaneTextField('botMessagesBackgroundColor', {
+                  label: 'Bot messages background color',
+                  onGetErrorMessage: this._validateColorPropertyAsync.bind(this), // validation function
+                  deferredValidationTime: 500 // delay after which to run the validation function
+                }),
+                PropertyPaneTextField('botMessagesForegroundColor', {
+                  label: 'Bot messages foreground color',
+                  onGetErrorMessage: this._validateColorPropertyAsync.bind(this), // validation function
+                  deferredValidationTime: 500 // delay after which to run the validation function
+                }),
+                PropertyPaneTextField('userMessagesBackgroundColor', {
+                  label: 'User messages background color',
+                  onGetErrorMessage: this._validateColorPropertyAsync.bind(this), // validation function
+                  deferredValidationTime: 500 // delay after which to run the validation function
+                }),
+                PropertyPaneTextField('userMessagesForegroundColor', {
+                  label: 'User messages foreground color',
+                  onGetErrorMessage: this._validateColorPropertyAsync.bind(this), // validation function
+                  deferredValidationTime: 500 // delay after which to run the validation function
                 })
               ]
             }
@@ -53,4 +90,15 @@ export default class SquireChatControlWebPart extends BaseClientSideWebPart<ISqu
       ]
     };
   }
+
+
+  private _validateColorPropertyAsync(value: string): string {
+    var colorRegex = /^([a-zA-Z0-9]){6}$/;
+    if (!value || colorRegex.test(value) == false) {
+      return "Please enter a valid 6 character hex color value";
+    }
+
+    return "";
+  }
+
 }
--- warroom/spfx/src/webparts/squireChatControl/components/ISquireChatControlProps.ts
+++
@@ -1,3 +0,0 @@
-export interface ISquireChatControlProps {
-  description: string;
-}
