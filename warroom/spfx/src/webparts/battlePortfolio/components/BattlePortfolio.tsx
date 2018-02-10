4a74aeaa1ebf5245771350b12c9ccfd40ec4f9c5
Thomas Granheim <Thomasog@puzzlepart.com>
2018-02-10 15:46:09 +0100

    battle portfolio

--- warroom/spfx/src/webparts/battlePortfolio/components/BattlePortfolio.tsx
+++ warroom/spfx/src/webparts/battlePortfolio/components/BattlePortfolio.tsx
@@ -5,7 +5,14 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import { GraphHttpClient, GraphHttpClientResponse, HttpClient, IHttpClientOptions } from '@microsoft/sp-http';
 import { WebPartTitle } from "@pnp/spfx-controls-react/lib/WebPartTitle";
-
+import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import {
+  Persona,
+  PersonaSize,
+  PersonaPresence
+} from 'office-ui-fabric-react/lib/Persona';
+import { DocumentCard, DocumentCardPreview, DocumentCardLocation, DocumentCardTitle, DocumentCardActivity, IDocumentCardPreviewProps } from 'office-ui-fabric-react/lib/DocumentCard';
+import { ImageFit } from 'office-ui-fabric-react/lib/Image';
 
 export interface IBattleRoomProperties {
   XP: number;
@@ -32,13 +39,41 @@ export default class BattlePortfolio extends React.Component<IBattlePortfolioPro
     this.fetchData();
   }
   public render(): React.ReactElement<IBattlePortfolioProps> {
+    console.log(this.state.groups);
+    let previewPropsUsingIcon: IDocumentCardPreviewProps = {
+      previewImages: [
+        {
+          height: 128
+
+        }
+      ]
+    };
+    previewPropsUsingIcon.previewImages[0].previewImageSrc = "/sites/wr/SiteAssets/img/war.png";
+    previewPropsUsingIcon.previewImages[0].imageFit = ImageFit.contain;
     let unitElements = this.state.groups.map((group) => {
       return (
-        <div className={styles.container}>
-          <div className={styles.unit}>
-            <div className={styles.details}>{group.displayName}</div>
-          </div>
-        </div>
+        <DocumentCard className={styles.card} onClick={() => window.location.href = `/sites/${group.mailNickname}`} >
+          <DocumentCardPreview { ...previewPropsUsingIcon } />
+          <DocumentCardTitle
+            title={group.displayName}
+            shouldTruncate={false}
+          />
+          <DocumentCardLocation
+            location={"Gold: " + group.techmikael_GenericSchema["ValueString05"]} />
+          <DocumentCardLocation
+            location={"XP: " + group.techmikael_GenericSchema["ValueString06"]} />
+          <DocumentCardLocation
+            location={"Vicories: " + group.techmikael_GenericSchema["ValueString07"]} />
+          <DocumentCardActivity
+            activity={"War Chieftains"}
+            people={
+              group.owners.map((owner) => {
+                return { name: owner.displayName, profileImageSrc: owner.imageUrl }
+
+              })
+            }
+          />
+        </DocumentCard>
       );
     });
     return (
@@ -52,13 +87,10 @@ export default class BattlePortfolio extends React.Component<IBattlePortfolioPro
   }
   private async fetchData(): Promise<void> {
     try {
-      let graphResponse = await this.props.context.graphHttpClient.get(`v1.0/groups/${this.props.context.pageContext.legacyPageContext.groupId}?$select=id,title,techmikael_GenericSchema`, GraphHttpClient.configurations.v1);
+      let graphResponse = await this.props.context.graphHttpClient.get(`v1.0/groups?$select=id,displayName,mailNickname,techmikael_GenericSchema&$filter=techmikael_GenericSchema/ValueString08 eq 'BattleRoom'&$expand=owners`, GraphHttpClient.configurations.v1);
       let response = await graphResponse.json();
-      // let newXP = +response.techmikael_GenericSchema["ValueString05"] + 500;
-      // let newGold = +response.techmikael_GenericSchema["ValueString06"] + 250;
-      // let battlesWon = +response.techmikael_GenericSchema["ValueString07"] + 1;
       this.setState({
-        groups: response,
+        groups: response.value,
         isLoading: false
       });
     } catch (error) {
